0640611a07a5b7a5536b9662c729d36b833c87f1
Paulo Souza <101979170+paulobsouza@users.noreply.github.com>
2025-07-06 17:13:43 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,29 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const alimentoRoutes = require('./routes/alimentoRoutes');
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log('Origem da Requisição Recebida:', origin);
-};
-
-app.use(cors(corsOptions));
-
 const app = express();
-const whitelist = [
-    'https://paulosouza.github.io', 
-    'http://localhost:5500',        
-    'http://127.0.0.1:5500'          
-]; 
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', alimentoRoutes);
