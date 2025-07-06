107d266dc82fcf475b3eea3dbc7619a36f62476c
Paulo Souza <101979170+paulobsouza@users.noreply.github.com>
2025-07-06 16:44:51 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,25 @@
-const express = require("express");
-const cors = require("cors");
-const alimentoRoutes = require("./routes/alimentoRoutes");
+const express = require('express');
+const cors = require('cors');
+const alimentoRoutes = require('./routes/alimentoRoutes');
 
 const app = express();
-const PORT = 3000;
 
-app.use(cors());
+const whitelist = ['https://paulobsouza.github.io/frontend/']; 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions)); 
 app.use(express.json());
+app.use('/api', alimentoRoutes);
 
-app.use("/api", alimentoRoutes);
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
